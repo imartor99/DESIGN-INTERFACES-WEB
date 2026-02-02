d23b784bce2f2b5d1589ac9437dfbf082784c7d8
imartor99 <imartor769@g.educaand.es>
2026-02-02 22:47:30 +0100

    proyecto fylo clase tailwind

---
+++ TEMA 8 - TAILWIND/Proyecto Fylo/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./js/**/*.js"],
+  darkMode: "selector",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
